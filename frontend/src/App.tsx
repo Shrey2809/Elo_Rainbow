00879df5cc9395cb871bbca17767c1d4ae0aa831
Siddharth Chhatbar <siddharth3901@gmail.com>
2024-09-13 17:28:31 -0600

    table struct

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,15 @@
+import EloTable from "./components/EloTable";
+
 function App() {
   return (
-    <>
-      <h1 className="text-red-700"> Hello, World </h1>
-    </>
+    <div className="flex flex-col items-center justify-center">
+      <h1 className="pt-16 text-slate-600 text-3xl">
+        Elo Rankings for Rainbow Six Siege
+      </h1>
+      <div className="w-[90%] flex flex-col items-center justify-center pt-8">
+        <EloTable />
+      </div>
+    </div>
   );
 }
 
