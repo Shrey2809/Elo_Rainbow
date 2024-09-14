33f136492809c91c618ee643c9ccf4a55b4bf265
Siddharth Chhatbar <siddharth3901@gmail.com>
2024-09-14 00:34:11 -0600

    fix: utils

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -4,15 +4,3 @@ import { twMerge } from "tailwind-merge";
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
-
-export function promisify<T>(
-  fn: (callback: (err: any, result: T) => void) => void,
-): (...args: any[]) => Promise<T> {
-  return (...args: any[]) =>
-    new Promise<T>((resolve, reject) => {
-      fn((err, result) => {
-        if (err) reject(err);
-        else resolve(result);
-      });
-    });
-}
