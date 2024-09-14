ddde735b4a7f5326e7e645d3c7cb035a2983b2a6
Siddharth Chhatbar <siddharth3901@gmail.com>
2024-09-14 01:16:16 -0600

    sm screen

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import EloTable from "./components/EloTable";
 function App() {
   return (
     <div className="flex flex-col items-center justify-center bg-myColor text-white font-sans">
-      <h1 className="pt-10 text-white text-3xl">
+      <h1 className="pt-10 text-white text-2xl md:text-3xl lg:text-4xl text-center px-4 sm:px-6">
         Elo Rankings for Rainbow Six Siege
       </h1>
       <div className="w-[90%] flex flex-col items-center justify-center pt-8">
