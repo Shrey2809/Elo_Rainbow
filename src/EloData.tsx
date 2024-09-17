09d78c97f7c1068372a6fbe6dccce9f511a5b9c7
Shrey <90151065+Shrey2809@users.noreply.github.com>
2024-09-16 20:50:16 -0600

    top bar refactor pt2

--- src/EloData.tsx
+++ src/EloData.tsx
@@ -1,10 +1,14 @@
 import EloTable from "./components/EloTable";
+import TopBar from "./components/TopBar";
 
 const MapData = () => {
   return (
-    <div className="flex flex-col items-center justify-center bg-myColor text-white font-sans">
-      <div className="w-[90%] flex flex-col items-center justify-center pt-8">
-        <EloTable />
+    <div>
+      <TopBar />
+      <div className="flex flex-col items-center justify-center bg-myColor text-white font-sans">
+        <div className="w-[90%] flex flex-col items-center justify-center pt-8">
+          <EloTable />
+        </div>
       </div>
     </div>
   );
--- src/MapData.tsx
+++ src/MapData.tsx
@@ -1,10 +1,14 @@
 import EloMapTable from "./components/EloMapTable";
+import TopBar from "./components/TopBar";
 
 const MapData = () => {
   return (
-    <div className="flex flex-col items-center justify-center bg-myColor text-white font-sans">
-      <div className="w-[90%] flex flex-col items-center justify-center pt-8">
-        <EloMapTable />
+    <div>
+      <TopBar />
+      <div className="flex flex-col items-center justify-center bg-myColor text-white font-sans">
+        <div className="w-[90%] flex flex-col items-center justify-center pt-8">
+          <EloMapTable />
+        </div>
       </div>
     </div>
   );
