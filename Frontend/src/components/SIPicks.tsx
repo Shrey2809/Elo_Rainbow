37cb4abf985373bb5ce89b7274c12fd483a37e2a
Shrey <90151065+Shrey2809@users.noreply.github.com>
2025-01-19 20:54:50 -0700

    mobile fixes

--- Frontend/src/components/SIPicks.tsx
+++ Frontend/src/components/SIPicks.tsx
@@ -67,83 +67,95 @@ export default function TierListMaker() {
 
   return (
     <div className="tier-list-container w-full bg-myColor p-8 items-center">
-      <div className="bg-myColor text-white p-4 rounded-2xl w-full ">
-        <div className="flex flex-col items-center justify-center">
-          <h1 className="text-4xl font-bold">SI 2025 Pick'ems</h1>
-          <p>Select the teams you think will place in each seed</p>
-        </div>
-      </div>
-
-      <div className="flex flex-col-2 items-center justify-center gap-16 ">
-        {Object.keys(groups).map((group) => (
-          <div key={group} className="group-container mt-8 w-fit bg-myDarkColor p-4 text-center rounded-3xl">
-            <h2 className="text-2xl font-bold text-myThirdColor ">{group}</h2>
-            <div className="all-teams mt-4">
-              <div className="flex flex-row justify-center gap-4 bg-mySecondCellColor rounded-3xl">
-                {groups[group].map((team) => (
-                  <div key={team} className="p-2 rounded-md text-white">
-                    <img
-                      src={`/team_logos/${logoMap[team]}`}
-                      alt={`${team} logo`}
-                      className="inline-block w-12 h-12 mr-2"
-                    />
-                  </div>
-                ))}
-              </div>
-            </div>
-
-            <div className="flex flex-col gap-4 pt-4">
-              {tiers.map((tier) => (
-                <div key={tier} className="tier-card bg-mySecondCellColor p-4 rounded-3xl">
-                  <h3 className="text-xl font-semibold text-white">{tier}</h3>
-                  {selections[group][tier] ? (
-                    <div className="rounded-md text-white flex items-center justify-center">
-                      <img
-                        src={`/team_logos/${logoMap[selections[group][tier]]}`}
-                        alt={`${selections[group][tier]} logo`}
-                        className="w-12 h-12"
-                      />
-                    </div>
-                  ) : (
-                    <Popover>
-                      <PopoverTrigger asChild>
-                        <button className="mt-2 bg-myFifthColor text-myDarkColor font-bold px-4 py-2 rounded-2xl">
-                          Pick Team
-                        </button>
-                      </PopoverTrigger>
-                      <PopoverContent className="bg-mySecondCellColor p-4 rounded-2xl">
-                        <div className="flex flex-col space-y-2">
-                          {groups[group].map((team) => (
-                            <button
-                              key={team}
-                              onClick={() => handleTeamChange(group, tier, team)}
-                              className="text-white px-4 py-2 rounded-xl hover:bg-mySecondaryColor w-full"
-                              disabled={Object.values(selections[group]).includes(team)}
-                            >
-                              <img
-                                src={`/team_logos/${logoMap[team]}`}
-                                alt={`${team} logo`}
-                                className="inline-block w-12 h-12 mr-2 drop-shadow-xl"
-                              />
-                            </button>
-                          ))}
-                        </div>
-                      </PopoverContent>
-                    </Popover>
-                  )}
-                </div>
-              ))}
-            </div>
-
-            <button
-              onClick={() => handleClearPicks(group)}
-              className="mt-4 bg-red-500 text-white font-bold px-4 py-2 rounded-2xl hover:bg-red-600"
-            >
-              Clear Picks
-            </button>
-          </div>
-        ))}
-      </div>
+  <div className="bg-myColor text-white p-4 rounded-2xl w-full">
+    <div className="flex flex-col items-center justify-center">
+      <h1 className="text-4xl font-bold">SI 2025 Pick'ems</h1>
+      <p>Select the teams you think will place in each seed</p>
     </div>
+  </div>
+
+  {/* Responsive Group Container */}
+  <div className="flex flex-col sm:flex-col lg:flex-row items-center justify-center gap-8 lg:gap-16">
+    {Object.keys(groups).map((group) => (
+      <div
+        key={group}
+        className="group-container mt-8 w-full sm:w-fit bg-myDarkColor p-4 text-center rounded-3xl"
+      >
+        <h2 className="text-2xl font-bold text-myThirdColor">{group}</h2>
+        <div className="all-teams mt-4">
+          <div className="flex flex-row justify-center gap-4 bg-mySecondCellColor rounded-3xl">
+            {groups[group].map((team) => (
+              <div key={team} className="p-2 rounded-md text-white">
+                <img
+                  src={`/team_logos/${logoMap[team]}`}
+                  alt={`${team} logo`}
+                  className="inline-block w-12 h-12 mr-2"
+                />
+              </div>
+            ))}
+          </div>
+        </div>
+
+        <div className="flex flex-col gap-4 pt-4">
+          {tiers.map((tier) => (
+            <div
+              key={tier}
+              className="tier-card bg-mySecondCellColor p-4 rounded-3xl"
+            >
+              <h3 className="text-xl font-semibold text-white">{tier}</h3>
+              {selections[group][tier] ? (
+                <div className="rounded-md text-white flex items-center justify-center">
+                  <img
+                    src={`/team_logos/${logoMap[selections[group][tier]]}`}
+                    alt={`${selections[group][tier]} logo`}
+                    className="w-12 h-12"
+                  />
+                </div>
+              ) : (
+                <Popover>
+                  <PopoverTrigger asChild>
+                    <button className="mt-2 bg-myFifthColor text-myDarkColor font-bold px-4 py-2 rounded-2xl">
+                      Pick Team
+                    </button>
+                  </PopoverTrigger>
+                  <PopoverContent className="bg-mySecondCellColor p-4 rounded-2xl">
+                    <div className="flex flex-col space-y-2">
+                      {groups[group].map((team) => (
+                        <button
+                          key={team}
+                          onClick={() =>
+                            handleTeamChange(group, tier, team)
+                          }
+                          className="text-white px-4 py-2 rounded-xl hover:bg-mySecondaryColor w-full"
+                          disabled={Object.values(selections[group]).includes(
+                            team
+                          )}
+                        >
+                          <img
+                            src={`/team_logos/${logoMap[team]}`}
+                            alt={`${team} logo`}
+                            className="inline-block w-12 h-12 mr-2 drop-shadow-xl"
+                          />
+                        </button>
+                      ))}
+                    </div>
+                  </PopoverContent>
+                </Popover>
+              )}
+            </div>
+          ))}
+        </div>
+
+        <button
+          onClick={() => handleClearPicks(group)}
+          className="mt-4 bg-myFourthColor text-black font-bold px-4 py-2 rounded-2xl hover:bg-red-600"
+        >
+          Clear Picks
+        </button>
+      </div>
+    ))}
+  </div>
+</div>
+
   );
 }
